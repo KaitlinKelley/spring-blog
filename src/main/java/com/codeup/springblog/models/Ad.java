bd908adeeb0eba4227bd4ce0f37bb19734a5be75
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-02-11 14:39:14 -0600

    working on dogs exercise

---
+++ src/main/java/com/codeup/springblog/models/Ad.java
@@ -0,0 +1,50 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ads")
+public class Ad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 1500)
+    private String description;
+
+    @Column(nullable = false)
+    private String title;
+
+    public Ad(){}
+
+    public Ad(long id, String description, String title) {
+        this.id = id;
+        this.description = description;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/codeup/springblog/models/Dog.java
@@ -0,0 +1,62 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "dogs")
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (nullable = false, length = 3)
+    private int age;
+
+    @Column (nullable = false)
+    private String name;
+
+    @Column (nullable = false, name = "reside_state", length = 2)
+    private String resideState;
+
+    public Dog(){}
+
+    public Dog(long id, int age, String name, String resideState) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.resideState = resideState;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResideState() {
+        return resideState;
+    }
+
+    public void setResideState(String resideState) {
+        this.resideState = resideState;
+    }
+}
